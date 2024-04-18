f1db384f203cac134710c43e8842b1c219a5f5c9
IsaacYusuke <isaacyusuke@usp.br>
2024-04-18 20:25:57 -0300

    Views autorizada a alterar dados

--- Aplicacao1/backend/App1/mapa/views.py
+++ Aplicacao1/backend/App1/mapa/views.py
@@ -5,6 +5,8 @@ from .serializers import StadiumSerializer, SectionSerializer, RowSerializer, Se
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 class StadiumViewSet(viewsets.ModelViewSet):
     queryset = Stadium.objects.all()
@@ -18,6 +20,7 @@ class RowViewSet(viewsets.ModelViewSet):
     queryset = Row.objects.all()
     serializer_class = RowSerializer
 
+@method_decorator(csrf_exempt, name='dispatch')
 class SeatViewSet(viewsets.ModelViewSet):
     queryset = Seat.objects.all()
     serializer_class = SeatSerializer
